757650fef1a838806a2d4a9597aa888eafa745d6
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-12 16:16:31 +0900

    뷰스타그램 스타일 완료, 포스팅 리스트 데이터 붙이는중

---
+++ src/components/Container.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Post :data="data[0]" />
+    <!-- <Post :data="data" />
+    <Post :data="data" /> -->
+  </div>
+</template>
+
+<script>
+import Post from "./Post.vue";
+
+export default {
+  name: "Container",
+  components: {
+    Post,
+  },
+  props: {
+    data: Array,
+  },
+};
+</script>
---
+++ src/components/Post.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="post">
+    <div class="post-header">
+      <div
+        class="profile"
+        :style="{ backgroundImage: `url(${data.userImage})` }"
+      ></div>
+      <span class="profile-name">{{ data.name }}</span>
+    </div>
+    <div
+      class="post-body"
+      :style="{ backgroundImage: `url(${data.postImage})` }"
+    ></div>
+    <div class="post-content">
+      <p>{{ data.likes }} Likes</p>
+      <p><strong>글쓴이아이디</strong> {{ data.content }}</p>
+      <p class="date">{{ data.date }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props: {
+    data: Array,
+  },
+};
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  /*background-image: url("https://placeimg.com/100/100/arch");*/
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  /* background-image: url("https://placeimg.com/640/480/animals");*/
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
