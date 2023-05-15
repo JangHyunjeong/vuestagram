53bcd213d000f2192f792a8a8c158b2d1ad788f4
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-05-15 16:49:55 +0900

    mitt 설치 후 셋팅

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// mitt 라이브러리 셋팅
+import mitt from "mitt";
+let emitter = mitt();
+let app = createApp(App);
+
+// 자주쓰는 라이브러리 여기에 등록해도 됨
+// 그러면 vue 파일에서 import 해올 필요 없이, this.emiiter로 사용가능
+// 글로벌하게 emitter를 막 사용할라고 설정해둔거임
+app.config.globalProperties.emitter = emitter;
+
+app.mount("#app");
